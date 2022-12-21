0ab877ee16c790481dae9e8d061fd1e6c3a882c1
edgardleal <edgardleal@gmail.com>
2022-12-21 09:43:06 -0300

    wip: unit tests

--- test/test.js
+++ test/test.js
@@ -10,3 +10,11 @@ test('defaults', async t => {
 
   t.snapshot(stream.fileList, 'Generated files')
 })
+
+test('package.json', async t => {
+  const mockAnswers = { name: 'test2022' }
+  const stream = await sao.mock({ generator }, mockAnswers)
+  const content = await stream.readFile('package.json')
+  const parsed = JSON.parse(content)
+  t.is(parsed.name, 'test2022')
+})
